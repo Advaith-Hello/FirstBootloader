8884ac43a35fabe4fd6638d431f5cb92a7dc7f20
Advaith_Hello <advaith.anoopr@gmail.com>
2026-08-01 20:50:44 -0500

    Basic stdin

--- src/main.c
+++ src/main.c
@@ -1,10 +1,38 @@
 #include <uefi.h>
+#include <string.h>
 
-int main (int argc, char **argv) {
-  printf("Hello World!\n");
+
+
+static inline void hang() {
   while (1) {
     __asm__ __volatile__("hlt");
   }
+}
+
+
+int main (int argc, char **argv) 
+{
+  int i = 0;
+  char buffer[256];
+  printf("What is your name: ");
+  
+  while (1)
+  {
+    int key = getchar_ifany();
+
+    if (key) {
+      const char c = (char)key;
+      if (c == '\r') {break;}
+      printf("%c", c);
+      buffer[i] = c;
+      i += 1;
+    }
+  }
+
+  printf("\n");
+  printf("Hello, %s", buffer);
+
+  hang();
   return 0;
 }
 
