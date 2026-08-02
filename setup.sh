09d2e0f08fce806b8088b533f36bcc1cf0a87d49
Advaith_Hello <advaith.anoopr@gmail.com>
2026-08-02 16:45:29 -0500

    Setup fix 1

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-git clone "https://codeberg.org/bzt/posix-uefi" "$SCRIPT_DIR/../"
+mkdir -p "$SCRIPT_DIR/../lib/"
+git clone "https://codeberg.org/bzt/posix-uefi" "$SCRIPT_DIR/../lib/"
 ln -s "$SCRIPT_DIR/../posix-uefi/uefi" "$SCRIPT_DIR/uefi"
 
